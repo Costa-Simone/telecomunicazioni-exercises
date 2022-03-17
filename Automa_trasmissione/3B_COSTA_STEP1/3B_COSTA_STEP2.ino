d96ce8ca9588b1e47f69edbacce3afc4e4249257
costawski <s.costa.2235@vallauri.edu>
2022-03-17 09:52:01 +0100

    Compito(17-03-22)

--- Automa_trasmissione/3B_COSTA_STEP1/3B_COSTA_STEP2.ino
+++ Automa_trasmissione/3B_COSTA_STEP1/3B_COSTA_STEP2.ino
@@ -1,7 +1,10 @@
 #define TxPin 13
 
+//variabili di stato
 int Sp = 0;
 int Sf = 0;
+
+//variabili di appoggio
 int i = 0;
 int S = 2;
 int N =  8;
@@ -11,36 +14,39 @@ bool dato = true;
 int bit = 0;
 int Car = 'A';
 bool parita = false;
+unsigned long Tempo = 0; //memorizzare il tempo
   
 void setup() {
   pinMode(TxPin, OUTPUT);
   digitalWrite(TxPin, HIGH);
+  Sp = 0;
 }
 
 void loop() {
-  trasmetti();
+  trasmetti(P);
 }
 
-void trasmetti() {
-    while(dato) {
+void trasmetti(char P) {
+  if(Tempo == 0 || millis() - Tempo >= Tb) {
     switch(Sp) {
     case 0:
+      Tempo = 0;
       digitalWrite(TxPin, HIGH);
-      delay(100);
       Sf = 1;
+      parita = false;
       break;
      
      case 1:
+     Tempo = millis();
       i = 0;
       digitalWrite(TxPin, LOW);
-      delay(Tb);
       Sf = 2;
       break;
 
      case 2:
+      Tempo += Tb;
       bit = ((Car &(1<<i)) != 0);
       digitalWrite(TxPin, bit);
-      delay(Tb);
       parita ^= bit;
       i++;
       if(i == N && P == 'N') {
@@ -54,29 +60,25 @@ void trasmetti() {
       break;
 
      case 3:
+      Tempo += Tb;
       if(P == 'E') {
         digitalWrite(TxPin, parita);
-        delay(Tb);
       }
       else if(P == 'O') {
         parita = !parita;
         digitalWrite(TxPin, parita);
-        delay(Tb);
       }
       Sf = 4;
       break;
 
      case 4:
+      Tempo += Tb;
       digitalWrite(TxPin, HIGH);
-      if(i < S) {
-        i++;
-        delay(Tb);
-      }
-      else if(i == S) {
+      i++;
+
+      if(i == S) {
         Sf = 0;
       }
-      delay(Tb);
-     
       break;
    }
 
