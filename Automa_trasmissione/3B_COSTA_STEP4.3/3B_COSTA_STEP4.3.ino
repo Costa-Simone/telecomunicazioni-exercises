9ee3488c88cf941215b1e3458ac081cb449ccf72
costawski <s.costa.2235@vallauri.edu>
2022-04-09 09:45:00 +0200

    Lezione(09-04-22)

--- Automa_trasmissione/3B_COSTA_STEP4.3/3B_COSTA_STEP4.3.ino
+++ Automa_trasmissione/3B_COSTA_STEP4.3/3B_COSTA_STEP4.3.ino
@@ -9,13 +9,11 @@ const int attesa = 2000;
 bool Tx();
 
 //variabili di appoggio
-int i = 0;
-int j = 0;
 bool bit = false;
 char mex[] = { 'C', 'O', 'S', 'T', 'A', '\n'};
 bool parita = false;
 unsigned long Tempo = 0; //memorizzare il tempo
-bool impegno = false;
+bool impegno = true; //variabile per verificare se automa sta trasmettendo
 
 void setup() {
   pinMode(TxPin, OUTPUT);
@@ -23,7 +21,7 @@ void setup() {
 }
 
 void loop() {
-  if(Tx() == false) {
+  if(Tx() == false && Tempo != 0) {
       Tempo += attesa;
       impegno = true;
   }
@@ -33,9 +31,11 @@ bool Tx() {
   static int Sp = 0;
   static int Sf = 0;
   static char datoCatturato;
+  static int i = 0, j = 0;
+  
   if(Tempo == 0 || millis() > Tempo + Tb) {
     switch(Sp) {
-    case 0:
+    case 0: //idle
       digitalWrite(TxPin, HIGH);
       Sf = 1;
       parita = false;
@@ -46,36 +46,40 @@ bool Tx() {
       else {
         impegno = true;
       }
+      
       break;
      
-     case 1:
-      Tempo = millis();
+     case 1: //start
+      Tempo = millis(); //porta tempo al ms attuale
       i = 0;
       digitalWrite(TxPin, LOW);
       Sf = 2;
-      datoCatturato = mex[j];
+      datoCatturato = mex[j]; //cattura carattere da trasmettere
       impegno = true;
       break;
 
-     case 2:
+     case 2: //trasmissione
       Tempo += Tb;
-      bit = ((datoCatturato & (1<<i)) != 0);
-      digitalWrite(TxPin, bit);
+      bit = ((datoCatturato & (1<<i)) != 0); //trasmissione del carattere attuale
+      digitalWrite(TxPin, bit); //stampa del bit
       parita ^= bit;
       i++;
+      
       if(i == N && P == 'N') {
         Sf = 4;
-        i = 0;
+        i = 0; //azzeramento variabile posizione bit
       }
       else if(i == N && P != 'N') {
         Sf = 3;
         i = 0;
       }
+      
       impegno = true;
       break;
 
-     case 3:
+     case 3: //parita
       Tempo += Tb;
+      
       if(P == 'E') {
         digitalWrite(TxPin, parita);
       }
@@ -83,11 +87,12 @@ bool Tx() {
         parita = !parita;
         digitalWrite(TxPin, parita);
       }
+      
       Sf = 4;
       impegno = true;
       break;
 
-     case 4:
+     case 4: //stop
       Tempo += Tb;
       digitalWrite(TxPin, HIGH);
       i++;
@@ -96,20 +101,24 @@ bool Tx() {
         Sf = 5;
         j++;
       }
+      
       impegno = true; 
       break;
 
-     case 5:
+     case 5: //azzeramento
       Sf = 0;
       Tempo += Tb;
       
       if(datoCatturato == '\n') {
-          j = 0;
+          j = 0; //azzeramento variabile posizione stringa
       }
+      
       impegno = true;
       break;
    }
+   
    Sp = Sf;
   }
+  
   return impegno;
 }
